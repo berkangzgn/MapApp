f1e00dc36c92cfb074bc3a452af4ba8a067bc40e
berkangzgn <bgezgin@protonmail.com>
2021-11-08 11:24:42 +0300

    Play with maps

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,14 +34,22 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        /*
-         altitude - yükseklik
-         coordinate.latitude - enlem
-         coordinate.longitude - boylam
-         */
         
-        print(locations[0].coordinate.latitude)
-        print(locations[0].coordinate.longitude)
+        // altitude - yükseklik
+        // coordinate.latitude - enlem
+        // coordinate.longitude - boylam
+        
+        //print(locations[0].coordinate.latitude)
+        //print(locations[0].coordinate.longitude)
+        
+        let locaiton = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        // Konumu değiştirmek için
+            // Belirtilen bölgenin yükseklik ve genişliği. Zoom durumunda seçtiğimiz yerin büyüklüğünün değişmesi durumu. Eğer çok fazla zoom göstermesin istiyorsak 0.9, 0.9a çekebiliriz.
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            // Haritadaki bölge
+        let region = MKCoordinateRegion(center: locaiton, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 }
