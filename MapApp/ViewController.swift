ff45c9d712c23da070bcfd94827ad430b09a802d
berkangzgn <bgezgin@protonmail.com>
2021-11-08 10:09:57 +0300

    Import MapKit

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+        // Bir ekleme yapmadan önce "Cann't MKMapView in scope" diye hata veriyor. Onu   düzeltmek için "import MapKit" ekledik.
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+            // Delegate : Herhangi bir olay olduğunda bir nesnenin başka bir nesneye haber göndermesini sağlayan Design Pattern'dir.
+            // Bu satırda da mapView Delegate tanımı yaptık.
+        mapView.delegate = self
     }
 
 
