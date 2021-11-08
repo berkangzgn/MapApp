8f260a133e0791d7c9aca7b0a3ca728e587bc1ce
berkangzgn <bgezgin@protonmail.com>
2021-11-08 10:35:28 +0300

    Get location coordinates

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import MapKit
+    // Konum bilgilerimizi alması için
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
         // Bir ekleme yapmadan önce "Cann't MKMapView in scope" diye hata veriyor. Onu   düzeltmek için "import MapKit" ekledik.
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     
     
     override func viewDidLoad() {
@@ -20,8 +23,27 @@ class ViewController: UIViewController, MKMapViewDelegate {
             // Delegate : Herhangi bir olay olduğunda bir nesnenin başka bir nesneye haber göndermesini sağlayan Design Pattern'dir.
             // Bu satırda da mapView Delegate tanımı yaptık.
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        // KONUM ALMA
+            // Konum bilgilerini saptama için en iyi konum seçeneğini seçtik. kCL sonrasını değiştirip araştırmalı / denemelisin.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            // Kullanıcıya konum isteğini sorduk.
+        locationManager.requestWhenInUseAuthorization()
+            // Location güncellemeye başladık.
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        /*
+         altitude - yükseklik
+         coordinate.latitude - enlem
+         coordinate.longitude - boylam
+         */
+        
+        print(locations[0].coordinate.latitude)
+        print(locations[0].coordinate.longitude)
+    }
 
 }
 
