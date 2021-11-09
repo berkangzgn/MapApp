450ea3e853c8f9a8f6091d5dea82cf97db038078
berkangzgn <bgezgin@protonmail.com>
2021-11-09 16:16:42 +0300

    Get data in DB

--- MapApp/ListViewController.swift
+++ MapApp/ListViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+        // Dizi tanımları
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +23,43 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.dataSource = self
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
+        
+        giveData()
     }
+    
+    func giveData() {
+            // DB bağlantısı
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+            // DB isteği
+        let request = NSFetchRequest<NSFetchRequestResult> (entityName: "Place")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            // Any dizisi veriyor. Biz onu NSManagedObject dizisi haline getirmeye çalışıyoruz.
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                    // ForLoop girmeden diziyi boşaltıyoruz
+                nameArray.removeAll(keepingCapacity: false)
+                idArray.removeAll(keepingCapacity: false)
+                
+                    // Dizilerde dönerek bilgileri çekme ve tnaımlı dizileri doldurma
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "placeName") as? String {
+                        nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArray.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+        } catch {
+            print("false")
+        }
+    }
+    
  
     @objc func addButtonClicked() {
         performSegue(withIdentifier: "toMapsVC", sender: nil)
@@ -26,12 +67,13 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return idArray.count
     }
 
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = nameArray[indexPath.row]
         return cell
     }
 }
